2cbe4329b8b67e05b4154ecbe3c1741876dd35dd
abc <rickykei@yahoo.com.hk>
2023-11-17 22:43:11 +0800

    20231117

--- config_db.php
+++ config_db.php
@@ -3,26 +3,30 @@
 use MongoDB\BSON\ObjectID;
 require_once('vendor/autoload.php'); 
 date_default_timezone_set('Asia/Hong_Kong');
-$domain="https://bea.e-profile.digital/";
-$mongourl="mongodb://cardbiz:cardbiz98014380@localhost:27017/cardbiz_db?authSource=admin";
+$domain="https://ebcard.hkbea.digital/";
+$mongourl="mongodb://cardbiz:cardbiz98014380@localhost:27017/cardbiz_bea_db?authSource=admin";
 $m = new MongoDB\Driver\Manager($mongourl);
 $dbclient = new MongoDB\Client($mongourl);
-$collection = $dbclient->selectDatabase("cardbiz_db")->selectCollection("staffs");
+$collection = $dbclient->selectDatabase("cardbiz_bea_db")->selectCollection("staffs");
  
-		if($uid!=""){
-			$abk=[['$match' => ['smartcard_uid' => $uid]], ['$lookup' => ['from' => 'companies', 'localField' => 'company_id', 'foreignField' => '_id', 'as' => 'company_detail']]];
-			//find smartcard valid or not
-			$coll_smartcard = $dbclient->selectDatabase("cardbiz_db")->selectCollection("smartcards");
-			$smartcard_filter=[['$match' => ['uid' => $uid]]];
-			$results_smartcard = $coll_smartcard->aggregate($smartcard_filter);
-			 
-			foreach ($results_smartcard as $doc) {
-				$smartcard_status=$doc->status;
-			}
-		}	else if($sig!=""){
+if($uid!=""){
+	  $collection2 = $dbclient->selectDatabase("cardbiz_bea_db")->selectCollection("smartcards");
+                        $abk2=[['$match' => ['uid' => $uid]], ['$lookup' => ['from' => 'staffs', 'localField' => '_id', 'foreignField' => 'smartcard_uid', 'as' => 'company_detail']]];
+                        //$abk=[['$match' => ['smartcard_uid' => $uid]], ['$lookup' => ['from' => 'companies', 'localField' => 'company_id', 'foreignField' => '_id', 'as' => 'company_detail']]];
+                        //find smartcard valid or not
+
+                        $results2 = $collection2->aggregate($abk2);
+                        foreach ($results2 as $document) {
+
+                                $sig=$document->company_detail[0]['_id'];
+                        }
+
+}	
+
+if($sig!=""){
 			$abk=[['$match' => ['_id' => new ObjectId($sig)]], ['$lookup' => ['from' => 'companies', 'localField' => 'company_id', 'foreignField' => '_id', 'as' => 'company_detail']]];
 		}
-		$results = $collection->aggregate($abk);
+$results = $collection->aggregate($abk);
 	
 
 
