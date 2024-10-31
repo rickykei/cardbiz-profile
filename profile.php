50cd5aa159c82c552dfc03bbbe112dd8b93e34e5
abc <rickykei@yahoo.com.hk>
2024-10-31 13:48:54 +0800

    20241031

--- profile.php
+++ profile.php
@@ -6,7 +6,11 @@ include_once("include_updateProfileCnt.php");
 if ($minisite_option=="2")
     include_once('index3.php');                 
 else if ($minisite_option=="3")
-    include_once('index4.php');  
+    include_once('index4.php');
+    else if ($minisite_option=="4")
+    include_once('index5.php');
+    else if ($minisite_option=="6")
+    include_once('index6.php');  
 else
     include_once('index2.php');                 
 ?>
