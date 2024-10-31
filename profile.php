7c5e15bbcd1193eefcfe0cfa3db140c535af64b2
abc <rickykei@yahoo.com.hk>
2024-10-31 14:50:14 +0800

    202410311200

--- profile.php
+++ profile.php
@@ -9,7 +9,7 @@ else if ($minisite_option=="3")
     include_once('index4.php');
     else if ($minisite_option=="4")
     include_once('index5.php');
-    else if ($minisite_option=="6")
+    else if ($minisite_option=="5")
     include_once('index6.php');  
 else
     include_once('index2.php');                 
